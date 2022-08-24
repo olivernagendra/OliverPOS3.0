57f841b131794cd60da93e263a1532dad5f6dcd5
nmscrossdev <78338383+nmscrossdev@users.noreply.github.com>
2022-08-24 16:34:02 +0530

    update plroduct Loader

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import AngledBracket_Left_Blue from '../../images/svg/AngledBracket-Left-Blue.svg'
 import AngledBracket_Right_Grey from '../../images/svg/AngledBracket-Right-Grey.svg'
 import Store_Icon_White from '../../images/svg/Store-Icon-White.svg'
-import { location } from '../../components/location/locationSlice';
-import { get_UDid, get_userName } from '../../components/common/localSettings';
+import { location } from './locationSlice';
+import { get_UDid, get_userName } from '../common/localSettings';
 
 import STATUSES from "../../constants/apiStatus";
 import { useNavigate } from 'react-router-dom';
