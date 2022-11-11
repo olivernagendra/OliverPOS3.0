4af25b053f3eb485a8e648f6d6b5445e078c0573
nmscrossdev <78338383+nmscrossdev@users.noreply.github.com>
2022-11-11 14:36:35 +0530

    delete notes slice

--- src/components/customer/CustomerSlice.js
+++ src/components/customer/CustomerSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { saveAPI, updateAPI, getDetailAPI, getPageAPI, getAllEventsAPi, updateCustomerNoteAPI, updateCreditScoreAPI, saveCustomerToTempOrderAPI ,getCountryListAPI,getStateListAPI } from './CustomerAPI'
+import { saveAPI, updateAPI, getDetailAPI, getPageAPI, getAllEventsAPi, updateCustomerNoteAPI, updateCreditScoreAPI, saveCustomerToTempOrderAPI ,getCountryListAPI,getStateListAPI ,deleteCustomerNoteAPI } from './CustomerAPI'
 import STATUSES from '../../constants/apiStatus';
 import { useIndexedDB } from 'react-indexed-db';
 
@@ -46,6 +46,18 @@ export const updateCreditScore = createAsyncThunk(
 );
 
 
+
+export const deleteCustomerNote = createAsyncThunk(
+  'deleteCustomerNote/deleteCustomerNoteAPI',
+  async (parameter, { rejectWithValue }) => {
+    try {
+      const response = await deleteCustomerNoteAPI(parameter);
+      return response;
+    } catch (err) {
+      return rejectWithValue(err.response.data)
+    }
+  }
+);
 
 export const updateCustomerNote = createAsyncThunk(
   'updateCustomerNote/updateCustomerNoteAPI',
@@ -428,6 +440,39 @@ export const updateCustomerNoteSlice = createSlice({
 export const { } = updateCustomerNoteSlice.actions;
 
 
+export const deleteCustomerNoteSlice = createSlice({
+  name: 'deleteCustomerNote',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteCustomerNote.pending, (state) => {
+        state.status = STATUSES.LOADING;
+        state.data = "";
+        state.error = "";
+        state.is_success = false;
+      })
+      .addCase(deleteCustomerNote.fulfilled, (state, action) => {
+        state.status = action.payload && action.payload.is_success == true ? STATUSES.IDLE : STATUSES.ERROR;
+        state.data = (action.payload && action.payload.is_success == true ? action.payload : "");
+        state.error = action.payload && action.payload.is_success == false ? action.payload.exceptions[0] : action.payload ? "Fail to fetch" : "";;
+        state.is_success = action.payload && action.payload.is_success == true ? true : false;
+      })
+      .addCase(deleteCustomerNote.rejected, (state, action) => {
+        state.status = STATUSES.IDLE;
+        state.data = "";
+        state.error = action.error;
+        state.is_success = false;
+      })
+  },
+});
+export const { } = deleteCustomerNoteSlice.actions;
+
+
 
 export const updateCreditScoreSlice = createSlice({
   name: 'updateCreditScore',
@@ -493,6 +538,7 @@ export const saveCustomerToTempOrderSlice = createSlice({
         state.is_success = false;
       })
       .addCase(saveCustomerToTempOrder.fulfilled, (state, action) => {
+        console.log("action",action)
         state.status = action.payload && action.payload.is_success == true ? STATUSES.IDLE : STATUSES.ERROR;
         state.data = (action.payload && action.payload.is_success == true ? action.payload : "");
         state.error = action.payload && action.payload.is_success == false ? action.payload.exceptions[0] : action.payload ? "Fail to fetch" : "";;
@@ -508,5 +554,5 @@ export const saveCustomerToTempOrderSlice = createSlice({
 });
 export const { } = saveCustomerToTempOrderSlice.actions;
 
-export default { CustomerSaveSlice, CustomerUpdateSlice, CustomerGetDetailsSlice, CustomergetPageSlice, getAllEventsSlice, updateCustomerNoteSlice, updateCreditScoreSlice, saveCustomerToTempOrderSlice,getCountrySlice ,getStateSlice};
+export default { CustomerSaveSlice, CustomerUpdateSlice, CustomerGetDetailsSlice, CustomergetPageSlice, getAllEventsSlice, updateCustomerNoteSlice,deleteCustomerNoteSlice, updateCreditScoreSlice, saveCustomerToTempOrderSlice,getCountrySlice ,getStateSlice};
 
