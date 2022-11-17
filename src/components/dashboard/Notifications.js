7886988ec9794c7bb7033f55029c509eb3b08b6d
Nagendra Suryawanshi <78338383+nmscrossdev@users.noreply.github.com>
2022-11-17 14:38:22 +0530

    update

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -81,7 +81,10 @@ const Notifications = (props) => {
         TempOrders && TempOrders.map(list => {
             var description = "";
             //order completed
+            // const d = new Date(list.date);
 
+            const _openDateTime = list.date && list.date !== "" ? list.date.split(',')[1] : "";
+            console.log("_openDateTime", _openDateTime)
             if (list.order_status == "completed" && list.new_customer_email !== "" && list.isCustomerEmail_send == true) {
                 description = ((
                     <div className="notification-card" key={uniqueKey()}>
@@ -97,7 +100,7 @@ const Notifications = (props) => {
                             <p className="style1">Order Created</p>
                             <p className="style2">Order #{list.TempOrderID}</p>
                         </div>
-                        <p>1hr ago</p>
+                        <p>{_openDateTime}</p>
                     </div>
 
                 ))
@@ -128,7 +131,7 @@ const Notifications = (props) => {
                         <p className="style1">Order Created</p>
                         <p className="style2">Order #{list.TempOrderID}</p>
                     </div>
-                    <p>1hr ago</p>
+                    <p>{_openDateTime}</p>
                 </div>))
             }
             //order refunded successfully
@@ -148,7 +151,7 @@ const Notifications = (props) => {
                             Order# {list.TempOrderID}
                         </p>
                     </div>
-                    <p>1hr ago</p>
+                    <p>{_openDateTime}</p>
                 </div>))
             }//sync issue
             else if (list.Status == "failed" && list.new_customer_email == "") {
@@ -163,7 +166,7 @@ const Notifications = (props) => {
                             <p className="style2">Order #{list.TempOrderID}</p>
                             <a href="#" onClick={() => reSyncOrder(list.TempOrderID)}>Retry</a>
                         </div>
-                        <p>3:23PM</p>
+                        <p>{_openDateTime}</p>
                         {/* <div className="side-color"></div>
                     <div className="main-row">
                         <img src={Error_Icon} alt="" />
@@ -296,7 +299,7 @@ const Notifications = (props) => {
     }, []);
     return (
         <div id="notificationsWrapper" className={props.isShow === true ? "notifications-wrapper" : "notifications-wrapper hidden"} onClick={(e) => outerClick(e)}>
-            <div id="notificationsContent" className={isSoundNotification === true ?"notifications settings":"notifications"}>
+            <div id="notificationsContent" className={isSoundNotification === true ? "notifications settings" : "notifications"}>
                 {/* <div id="soundNotificationsWrapper" className={isSoundNotification === true ? "sound-notifications-wrapper" : "sound-notifications-wrapper hidden"}>
                     <div className="sound-notifications">
                         <div className="header">
@@ -412,33 +415,33 @@ const Notifications = (props) => {
                     </div> */}
                 </div>
                 <div className="noti-settings-background" id="notiSettingsBackground"></div>
-					<div className="noti-settings-wrapper">
-						<div class="noti-settings">
-							<div class="header">
-								<p>Settings</p>
-							</div>
-							<div class="settings-list">
-								<div class="setting-row">
-									<img src={VolumeIcon} alt="" />
-									<p>Sound Notifications</p>
-								</div>
-								<div class="setting-row">
-									<label>
-										POS Orders
-										<input type="checkbox"/>
-										<div class="toggle"></div>
-									</label>
-								</div>
-								<div class="setting-row">
-									<label>
-										Web Order
-										<input type="checkbox"/>
-										<div class="toggle"></div>
-									</label>
-								</div>
-							</div>
-						</div>
-					</div>
+                <div className="noti-settings-wrapper">
+                    <div class="noti-settings">
+                        <div class="header">
+                            <p>Settings</p>
+                        </div>
+                        <div class="settings-list">
+                            <div class="setting-row">
+                                <img src={VolumeIcon} alt="" />
+                                <p>Sound Notifications</p>
+                            </div>
+                            <div class="setting-row">
+                                <label>
+                                    POS Orders
+                                    <input type="checkbox" />
+                                    <div class="toggle"></div>
+                                </label>
+                            </div>
+                            <div class="setting-row">
+                                <label>
+                                    Web Order
+                                    <input type="checkbox" />
+                                    <div class="toggle"></div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>)
 }
